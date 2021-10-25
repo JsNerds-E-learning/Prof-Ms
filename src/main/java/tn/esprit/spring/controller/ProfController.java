d517619417e30f1accb5c614d10ab39fefa2b3af
alextrazah <mohamedaziz.sahnoun@esprit.tn>
2021-10-25 10:54:48 +0100

    delete grade

--- src/main/java/tn/esprit/spring/controller/ProfController.java
+++ src/main/java/tn/esprit/spring/controller/ProfController.java
@@ -2,6 +2,7 @@ package tn.esprit.spring.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import tn.esprit.spring.entity.Grade;
 import tn.esprit.spring.entity.Prof;
-import tn.esprit.spring.service.GradeService;
 import tn.esprit.spring.service.ProfService;
 
 @RestController
 public class ProfController {
 @Autowired 
 ProfService profService;
-@Autowired
-GradeService gradeService;
-@RequestMapping(value = "/actuator/info")
+
 @GetMapping("/retrieve-all-users")
 @ResponseBody
 public List<Prof> getUsers(){
@@ -40,9 +37,9 @@ public Prof getUserById(@PathVariable("id") String id_user){
 @PostMapping("/add-prof")
 @ResponseBody
 public String addUser(@RequestBody Prof u){
-	Grade r= gradeService.addRole(u.getRole());
+
 	Prof prof= profService.addUser(u);
-	return "this is prof:"+prof+"and this is his prof:"+r ;
+	return "this is prof:"+prof;
 }
 
 @DeleteMapping("/delete-prof{id}")
--- src/main/java/tn/esprit/spring/entity/Prof.java
+++ src/main/java/tn/esprit/spring/entity/Prof.java
@@ -30,96 +30,71 @@ public class Prof implements Serializable {
 	private String adresse_user;
 	private String sexe_user;
 	private float salaire_user;
+	private String grade;
+	public long getId_user() {
+		return id_user;
+	}
+	public void setId_user(long id_user) {
+		this.id_user = id_user;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getMail_user() {
+		return mail_user;
+	}
+	public void setMail_user(String mail_user) {
+		this.mail_user = mail_user;
+	}
+	public long getTel_user() {
+		return tel_user;
+	}
+	public void setTel_user(long tel_user) {
+		this.tel_user = tel_user;
+	}
+	public String getAdresse_user() {
+		return adresse_user;
+	}
+	public void setAdresse_user(String adresse_user) {
+		this.adresse_user = adresse_user;
+	}
+	public String getSexe_user() {
+		return sexe_user;
+	}
+	public void setSexe_user(String sexe_user) {
+		this.sexe_user = sexe_user;
+	}
+	public float getSalaire_user() {
+		return salaire_user;
+	}
+	public void setSalaire_user(float salaire_user) {
+		this.salaire_user = salaire_user;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	@Override
+	public String toString() {
+		return "Prof [id_user=" + id_user + ", firstName=" + firstName + ", lastName=" + lastName + ", mail_user="
+				+ mail_user + ", tel_user=" + tel_user + ", adresse_user=" + adresse_user + ", sexe_user=" + sexe_user
+				+ ", salaire_user=" + salaire_user + ", grade=" + grade + "]";
+	}
+	
 
-
-	@OneToOne
-	@JoinColumn(name = "Grade_name")
-    private Grade grade;
-   @Temporal (TemporalType.DATE)
-   private Date date_naissance;
-public Grade getRole() {
-	return grade;
-}
-public void setRole(Grade grade) {
-	this.grade = grade;
-}
-public String getFirstName() {
-	return firstName;
-}
-public void setFirstName(String firstName) {
-	this.firstName = firstName;
-}
-public String getLastName() {
-	return lastName;
-}
-public void setLastName(String lastName) {
-	this.lastName = lastName;
-}
-public long getId_user() {
-	return id_user;
-}
-public void setId_user(long id_user) {
-	this.id_user = id_user;
-}
-public String getMail_user() {
-	return mail_user;
-}
-public void setMail_user(String mail_user) {
-	this.mail_user = mail_user;
-}
-public long getTel_user() {
-	return tel_user;
-}
-public void setTel_user(long tel_user) {
-	this.tel_user = tel_user;
-}
-public String getAdresse_user() {
-	return adresse_user;
-}
-public void setAdresse_user(String adresse_user) {
-	this.adresse_user = adresse_user;
-}
-public String getSexe_user() {
-	return sexe_user;
-}
-public void setSexe_user(String sexe_user) {
-	this.sexe_user = sexe_user;
-}
-public float getSalaire_user() {
-	return salaire_user;
-}
-public void setSalaire_user(float salaire_user) {
-	this.salaire_user = salaire_user;
-}
-public Date getDate_naissance() {
-	return date_naissance;
-}
-public void setDate_naissance(Date date_naissance) {
-	this.date_naissance = date_naissance;
-}
-@Override
-public String toString() {
-	return "User [id_user=" + id_user + ", firstName=" + firstName + ", lastName=" + lastName + ", mail_user="
-			+ mail_user + ", tel_user=" + tel_user + ", adresse_user=" + adresse_user + ", sexe_user=" + sexe_user
-			+ ", salaire_user=" + salaire_user + ", role=" + grade + ", date_naissance=" + date_naissance + "]";
-}
-public Prof(long id_user, String firstName, String lastName, String mail_user, long tel_user, String adresse_user,
-		String sexe_user, float salaire_user, Grade grade, Date date_naissance) {
-	super();
-	this.id_user = id_user;
-	this.firstName = firstName;
-	this.lastName = lastName;
-	this.mail_user = mail_user;
-	this.tel_user = tel_user;
-	this.adresse_user = adresse_user;
-	this.sexe_user = sexe_user;
-	this.salaire_user = salaire_user;
-	this.grade = grade;
-	this.date_naissance = date_naissance;
-}
-public Prof() {
-	super();
-	// TODO Auto-generated constructor stub
-}
 
 }
